675766eec4375ac949621bdbccb190de1a99e230
Benedikt Werner <1benediktwerner@gmail.com>
2017-06-29 05:18:58 +0200

    Added enter button support

--- popup.js
+++ popup.js
@@ -1,24 +1,43 @@
 document.addEventListener("DOMContentLoaded", function() {
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 		if (tabs[0].url.match(/https?:\/\/www\.youtube\.com\/watch*/)) {
-			document.getElementById("yt").style.display = "block";
-			document.getElementById("tum").style.display = "none";
-			document.getElementById("yt-set-speed").onclick = function() {
-				var val = document.getElementById("yt-speed").value;
-				chrome.tabs.executeScript(tabs[0].id, { code: "document.getElementsByClassName('video-stream')[0].playbackRate="+val+";" });
-			};
+			setupYTButton(tabs[0]);
 		}
 		else if (tabs[0].url.match(/https?:\/\/streams\.tum\.de\/Mediasite\/Play\/*/)) {
-			document.getElementById("yt").style.display = "none";
-			document.getElementById("tum").style.display = "block";
-			document.getElementById("tum-add-speed").onclick = function() {
-				var val = document.getElementById("tum-speed").value;
-				console.log(val);
-				chrome.tabs.executeScript(tabs[0].id, {
-					code: "var s=document.createElement('script');s.appendChild(document.createTextNode('Mediasite.MediaPlayer.ViewOptions.playbackRates.push("+val+");'));(document.body||document.head||document.documentElement).appendChild(s);"
-				});
-			};
+			setupTUMBUtton(tabs[0]);
 		}
 	});
 		
 });
+
+function setupYTButton(tab) {
+	document.getElementById("yt").style.display = "block";
+	document.getElementById("tum").style.display = "none";
+	document.getElementById("yt-set-speed").onclick = function() {onYTClick(tab);};
+	document.getElementById("yt-speed").addEventListener("keyup", function(e) {
+		e.preventDefault();
+		if (e.keyCode == 13)
+			onYTClick(tab);
+	});
+}
+
+function onYTClick(tab) {
+	var val = document.getElementById("yt-speed").value;
+	chrome.tabs.executeScript(tab.id, {
+		code: "document.getElementsByClassName('video-stream')[0].playbackRate="+val+";"
+	});
+	window.close();
+};
+
+function setupTUMButton(tab) {
+	document.getElementById("yt").style.display = "none";
+	document.getElementById("tum").style.display = "block";
+	document.getElementById("tum-add-speed").onclick = function() {
+		var val = document.getElementById("tum-speed").value;
+		console.log(val);
+		chrome.tabs.executeScript(tabs[0].id, {
+			code: "var s=document.createElement('script');s.appendChild(document.createTextNode('Mediasite.MediaPlayer.ViewOptions.playbackRates.push("+val+");'));(document.body||document.head||document.documentElement).appendChild(s);"
+		});
+		window.close();
+	};
+}
